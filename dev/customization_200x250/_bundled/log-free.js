9baa4cb96a8c84ef58148a866489a6cb5fbc4c15
Gar <gar@lonelydatu.com>
2020-10-15 13:10:55 -0400

    on

--- dev/customization_200x250/_bundled/log-free.js
+++ dev/customization_200x250/_bundled/log-free.js
@@ -5,11 +5,11 @@ var _extends = Object.assign || function (target) { for (var i = 1; i < argument
 
 var zero = { opacity: 0 };
 var one = { opacity: 1 };
-var down = { y: "+=70" };
-var up = { y: "-=70" };
+var down = { x: "+=40", opacity: 0 };
+var up = { x: "-=70", opacity: 0 };
 
 function start() {
-	TweenLite.defaultEase = Power4.easeOut;
+	TweenLite.defaultEase = Power3.easeOut;
 	var tl = new TimelineMax();
 
 	tl.set(".frame1", _extends({}, one));
@@ -18,39 +18,24 @@ function start() {
 	tl.add(exit(1), "+=2");
 
 	tl.add(enter(2));
+	tl.set(".product_" + 1, { opacity: 0 });
+
 	tl.add(exit(2), "+=2");
 
 	tl.to(".logo", .3, { opacity: 0 });
 	tl.from(".logo2", .3, { opacity: 0 });
 	tl.add(enter(3));
-
-	tl.add("3-out", "+=2");
-	tl.to(".product_" + 3, .5, _extends({}, down), "3-out+=.1");
-	tl.to(".bg_" + 3, .5, { clip: "rect(90px, 728px, 90px, 728px)" }, "3-out");
-
-	tl.add("4-in");
-	tl.from(".product_" + 4, .5, _extends({}, up), "4-in");
-	tl.from(".bg_" + 4, 1, { clip: "rect(0px, 0px, 90px, 0px)" });
-
-	tl.add("4-out", "+=1");
-	tl.to(".t" + 3, .5, _extends({}, down), "4-out");
-	tl.to(".bg_" + 4, .5, { clip: "rect(90px, 728px, 90px, 728px)" });
-	tl.from(".cta", .5, _extends({}, up));
-
-	// tl.add(exit(3), "+=1")
-
-	// tl.add(enter(4))
-	// tl.add(exit(4), "+=1")
-
-	// tl.gotoAndPlay("wave2")
+	tl.set(".product_" + 2, { opacity: 0 });
+	tl.to(".bg_" + 3, .7, { opacity: 0 }, "+=.3");
+	tl.from(".cta", .5, { opacity: 0, y: "+=30" });
 }
 
 function exit(num) {
 	var tl = new TimelineMax();
 	tl.add("exit");
-	tl.to(".t" + num, .3, _extends({}, down), "exit");
-	tl.to(".product_" + num, .3, _extends({}, down), "exit+=.1");
-	tl.to(".bg_" + num, .5, { clip: "rect(90px, 728px, 90px, 728px)" }, "exit");
+	tl.to(".bg_" + num, .5, { clip: "rect(0px, 200px, 250px, 200px)" }, "exit");
+	tl.to(".t" + num, .3, _extends({}, down), "exit+=.2");
+	// tl.to(`.product_${num}`, .3, {...down}, `exit+=.1`)
 	return tl;
 }
 
@@ -58,9 +43,9 @@ function enter(num) {
 	var tl = new TimelineMax();
 	tl.add("enter");
 	tl.from(".t" + num, .4, _extends({}, up), "enter");
-	tl.from(".product_" + num, .4, _extends({}, up), "enter+=.1");
-	tl.from(".bg_" + num, .7, { clip: "rect(0px, 0px, 90px, 0px)" }, "enter");
+	tl.from(".product_" + num, .4, { clip: "rect(0px, 240px, 500px, 240px)" }, "enter+=.1");
 
+	tl.from(".bg_" + num, .7, { clip: "rect(0px, 0px, 250px, 0px)" }, "enter");
 	return tl;
 }
 
