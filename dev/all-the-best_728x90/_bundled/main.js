c52af281d3d70d40cb2d466a5781198874ec53c5
Gar <gar@lonelydatu.com>
2020-10-14 22:40:05 -0400

    on

--- dev/all-the-best_728x90/_bundled/main.js
+++ dev/all-the-best_728x90/_bundled/main.js
@@ -1,14 +1,47 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 "use strict";
 
-function start() {
-	TweenLite.defaultEase = Power2.easeOut;
-	var tl = new TimelineMax({ repeat: 1, repeatDelay: 2.5 });
-	tl.set(".frame1", { opacity: 1 });
-	tl.from(".t1", .5, { opacity: 0 }, "+=.2");
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
-	tl.to(".t1", .3, { opacity: 0 }, "+=1.7");
-	tl.from(".t2", .3, { opacity: 0 }, "+=.1");
+function start() {
+	TweenLite.defaultEase = Power3.easeInOut;
+	var tl = new TimelineMax();
+
+	var fromO = { opacity: 0, y: "-=20" };
+	var toO = { opacity: 0, y: "+=20" };
+
+	tl.set(".frame1", { opacity: 1 });
+
+	tl.add('wave1');
+	tl.from(".wave", 1, { scale: "+=.5", x: "-150", clip: "rect(0px, 0px, 90px, 0px)" }, 'wave1');
+	tl.from(".bg2", 1.5, { clip: "rect(0px, 0px, 90px, 0px)" }, 'wave1');
+
+	tl.to(".wave", 1, { x: "+=200", opacity: 0, clip: "rect(0px, 728px, 90px, 728px)" }, "+=.2");
+	tl.set(".wave", { opacity: 1, scale: 1, x: 0, clip: "rect(0px, 0px, 90px, 0px)" });
+
+	tl.from(".t1", .3, _extends({}, fromO), '-=.3');
+	tl.to(".t1", .3, _extends({}, toO), "+=2");
+
+	tl.from(".t2", .3, _extends({}, fromO));
+	tl.add('t2-end', "+=2");
+	tl.to([".t2"], .3, _extends({}, toO), "t2-end");
+	tl.to([".logo"], .3, { opacity: 0 }, "t2-end");
+
+	tl.add("t3");
+	tl.from(".t3", .3, _extends({}, fromO), "t3");
+	tl.from(".logo2", .3, { opacity: 0 }, "t3");
+
+	tl.add("wave2", "+=1");
+
+	tl.to(".wave", 1, { clip: "rect(0px, 728px, 90px, 0px)" }, "wave2");
+	tl.to([".t3"], .3, _extends({}, toO), "wave2+=1");
+	tl.to([".logo2"], .3, { opacity: 0 }, "wave2+=1");
+	tl.to(".wave", 1, { x: "+=200", opacity: 0, clip: "rect(0px, 728px, 90px, 728px)" }, "wave2+=1");
+
+	tl.to(".logo", .3, { opacity: 1 });
+	tl.from(".t4", .3, _extends({}, fromO));
+
+	// tl.gotoAndPlay("wave2")
 }
 
 start();
--- dev/all-the-best_728x90/_js/main.js
+++ dev/all-the-best_728x90/_js/main.js
@@ -1,11 +1,48 @@
 function start(){
-	TweenLite.defaultEase = Power2.easeOut
-	const tl = new TimelineMax({repeat:1, repeatDelay:2.5})
+	TweenLite.defaultEase = Power3.easeInOut
+	const tl = new TimelineMax()
+
+	const fromO = {opacity:0, y:"-=20"}
+	const toO = {opacity:0, y:"+=20"}
+
 	tl.set(".frame1", {opacity:1})
-	tl.from(".t1", .5, {opacity:0}, "+=.2")
 	
-	tl.to(".t1", .3, {opacity:0}, "+=1.7")
-	tl.from(".t2", .3, {opacity:0}, "+=.1")
+	tl.add('wave1')
+	tl.from(".wave", 1, {scale:"+=.5", x:"-150", clip: `rect(0px, 0px, 90px, 0px)`}, 'wave1')
+	tl.from(".bg2", 1.5, {clip: `rect(0px, 0px, 90px, 0px)`}, 'wave1')
+
+	tl.to(".wave", 1, {x:"+=200", opacity:0, clip: `rect(0px, 728px, 90px, 728px)`}, "+=.2")
+	tl.set(".wave", {opacity:1, scale:1, x:0, clip: `rect(0px, 0px, 90px, 0px)`})
+
+	tl.from(".t1", .3, {...fromO}, '-=.3')
+	tl.to(".t1", .3, {...toO}, "+=2")
+
+	tl.from(".t2", .3, {...fromO})
+	tl.add('t2-end', "+=2")
+	tl.to([".t2"], .3, {...toO}, "t2-end")
+	tl.to([".logo"], .3, {opacity:0}, "t2-end")
+
+	tl.add("t3")
+	tl.from(".t3", .3, {...fromO}, "t3")
+	tl.from(".logo2", .3, {opacity:0}, "t3")
+	
+	tl.add("wave2", "+=1")
+	
+	tl.to(".wave", 1, { clip: `rect(0px, 728px, 90px, 0px)`}, "wave2")
+	tl.to([".t3"], .3, {...toO}, "wave2+=1")
+	tl.to([".logo2"], .3, {opacity:0}, "wave2+=1")
+	tl.to(".wave", 1, {x:"+=200", opacity:0, clip: `rect(0px, 728px, 90px, 728px)`}, "wave2+=1")
+
+	tl.to(".logo", .3, {opacity:1})
+	tl.from(".t4", .3, {...fromO})
+
+
+	// tl.gotoAndPlay("wave2")
+	
+
+
+
+
 }
 
 start()
