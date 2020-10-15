5a9139c9286e1eaa5f27f1c222e2d790e55ddee3
Gar <gar@lonelydatu.com>
2020-10-14 22:55:51 -0400

    on

--- dev/all-the-best_728x90/_bundled/log-free.js
+++ dev/all-the-best_728x90/_bundled/log-free.js
@@ -14,9 +14,9 @@ function start() {
 
 	tl.add('wave1');
 	tl.from(".wave", 1, { scale: "+=.5", x: "-150", clip: "rect(0px, 0px, 90px, 0px)" }, 'wave1');
-	tl.from(".bg2", 1.5, { clip: "rect(0px, 0px, 90px, 0px)" }, 'wave1');
+	tl.from(".bg2", 1, { clip: "rect(0px, 0px, 90px, 0px)" }, 'wave1');
 
-	tl.to(".wave", 1, { x: "+=200", opacity: 0, clip: "rect(0px, 728px, 90px, 728px)" }, "+=.2");
+	tl.to(".wave", 1, { x: "+=200", opacity: 0, clip: "rect(0px, 728px, 90px, 728px)" });
 	tl.set(".wave", { opacity: 1, scale: 1, x: 0, clip: "rect(0px, 0px, 90px, 0px)" });
 
 	tl.from(".t1", .3, _extends({}, fromO), '-=.3');
